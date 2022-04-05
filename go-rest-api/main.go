51e820cdbff10e3ea72eadca11643ee40a3aa009
Risyandi <risyandi.94@gmail.com>
2022-04-05 23:21:02 +0700

    fix: update main.go rest-api

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -1,68 +1,60 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-
+	// initialize the router
 	router := gin.Default()
 
-	// version 1
-	apiV1 := router.Group("/v1")
-
-	apiV1.GET("users", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "List Of V1 Users")
+	// group version 1
+	groupApiVersionOne := router.Group("/v1")
+	groupApiVersionOne.GET("users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "List Of group version one Users")
 	})
 
 	// User only can be added by authorized person
-	authV1 := apiV1.Group("/", AuthMiddleWare())
+	authVersionOne := groupApiVersionOne.Group("/", AuthMiddleWare())
+	authVersionOne.POST("users/add", AddVersionOne)
 
-	authV1.POST("users/add", AddV1User)
-
-	//version 2
-	apiV2 := router.Group("/v2")
-
-	apiV2.GET("users", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "List Of V2 Users")
+	// group version 2
+	groupApiVersionTwo := router.Group("/v2")
+	groupApiVersionTwo.GET("users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, "List Of group version two Users")
 	})
 
 	// User only can be added by authorized person
-	authV2 := apiV2.Group("/", AuthMiddleWare())
+	authVersionTwo := groupApiVersionTwo.Group("/", AuthMiddleWare())
+	authVersionTwo.POST("users/add", AddVersionTwo)
 
-	authV2.POST("users/add", AddV2User)
-
+	// Listen and Server in
 	_ = router.Run(":8081")
-
 }
 
-func AddV1User(c *gin.Context) {
-
-	// AddUser
-
+func AddVersionOne(c *gin.Context) {
+	// AddUser Version 1
 	c.JSON(http.StatusOK, "V1 User added")
 }
 
-func AddV2User(c *gin.Context) {
-
-	// AddUser
-
+func AddVersionTwo(c *gin.Context) {
+	// AddUser Version 2
 	c.JSON(http.StatusOK, "V2 User added")
 }
 
 func AuthMiddleWare() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		// here you can add your authentication method to authorize users.
 		username := c.PostForm("user")
 		password := c.PostForm("password")
 
+		// checking the username and password
 		if username == "foo" && password == "bar" {
 			return
 		} else {
 			c.AbortWithStatus(http.StatusUnauthorized)
 		}
-
 	}
 }
