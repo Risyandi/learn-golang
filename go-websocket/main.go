5a34ec5b4ce54e26f3f67bdae93863a118bd11c1
Risyandi <risyandi.94@gmail.com>
2022-11-10 15:25:41 +0700

    fix: adding event reply and typing

--- go-websocket/main.go
+++ go-websocket/main.go
@@ -9,53 +9,72 @@ import (
 )
 
 func main() {
+	// initialization the socket server
 	server := socketio.NewServer(nil)
 
-	// ** not broadcast to all connected
+	// handling socket event connected
+	// and set default not join in others room, when connected and send message not broadcast to all connected
 	// server.OnConnect("/", func(so socketio.Conn) error {
 	// 	so.SetContext("")
 	// 	fmt.Println("connected:", so.ID())
 	// 	return nil
 	// })
 
-	// ** broadcast to all connected
+	// handling socket event connected
+	// and default join in room with name "room" for broadcast to all connected with name room same
 	server.OnConnect("/chat", func(so socketio.Conn) error {
+		// set default context is empty and then join to room with name "room1"
 		so.SetContext("")
-		fmt.Println("chat user connected: ", so.ID())
+
+		// print on console log server
+		fmt.Println("id users connected: ", so.ID())
+
 		so.Join("room1")
 		return nil
 	})
 
-	// server event for socket.io
+	// handling socket event call nameEvent "reply"
 	server.OnEvent("/chat", "reply", func(so socketio.Conn, messageText string) {
+		// test case
+		// checkURL := so.URL()
+		// fmt.Println("url: ", checkURL)
+
+		// checkLocalAddr := so.LocalAddr()
+		// fmt.Println("LocalAddr: ", checkLocalAddr)
+
+		// not test case
+		fmt.Println("messageText: ", messageText)
 		so.Emit("reply", messageText)
+		// return messageText
 	})
 
-	server.OnEvent("/chat", "messageText", func(so socketio.Conn, messageText string) string {
+	// handling socket event call nameEvent "typing"
+	server.OnEvent("/chat", "typing", func(so socketio.Conn, messageText string) string {
 		nameSpace := so.Namespace()
-		server.BroadcastToRoom(nameSpace, "room1", "reply", messageText)
+		server.BroadcastToRoom(nameSpace, "room1", "typing", messageText)
 		return messageText
 	})
 
-	server.OnEvent("/", "bye", func(so socketio.Conn) string {
-		last := so.Context().(string)
-		so.Emit("bye", last)
-		so.Close()
-		return last
-	})
-
+	// handling socket event error
 	server.OnError("/chat", func(so socketio.Conn, message error) {
+		// print on console log server
+		// messageError := message
+		// fmt.Println("error: ", messageError)
 		fmt.Println("error: ", message)
 	})
 
+	// handling socket event disconnected
 	server.OnDisconnect("/chat", func(so socketio.Conn, reason string) {
+		// print on console log server
+		// messageReason := reason
+		// fmt.Println("closed: ", messageReason)
 		fmt.Println("closed: ", reason)
 	})
 
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/socket.io/", server)
+	http.Handle("/", server)
 	http.Handle("/", http.FileServer(http.Dir("./asset")))
 	log.Println("serving localhost on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
