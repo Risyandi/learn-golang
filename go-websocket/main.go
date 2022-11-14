69c882c109fe853d580b1d5089a33781c7bbfb87
Risyandi <risyandi.94@gmail.com>
2022-11-14 14:23:43 +0700

    fix: modified and test case

--- go-websocket/main.go
+++ go-websocket/main.go
@@ -22,13 +22,12 @@ func main() {
 
 	// handling socket event connected
 	// and default join in room with name "room" for broadcast to all connected with name room same
-	server.OnConnect("/chat", func(so socketio.Conn) error {
+	server.OnConnect("/", func(so socketio.Conn) error {
 		// set default context is empty and then join to room with name "room1"
 		so.SetContext("")
 
 		// print on console log server
 		fmt.Println("id users connected: ", so.ID())
-
 		so.Join("room1")
 		return nil
 	})
@@ -43,38 +42,42 @@ func main() {
 		// fmt.Println("LocalAddr: ", checkLocalAddr)
 
 		// not test case
-		fmt.Println("messageText: ", messageText)
 		so.Emit("reply", messageText)
-		// return messageText
 	})
 
 	// handling socket event call nameEvent "typing"
-	server.OnEvent("/chat", "typing", func(so socketio.Conn, messageText string) string {
+	server.OnEvent("/chat", "typing", func(so socketio.Conn, messageText string) {
+		so.Emit("typing", messageText)
+	})
+
+	// handling socket event call nameEvent "typing"
+	server.OnEvent("/", "broadcast", func(so socketio.Conn, messageText string) {
 		nameSpace := so.Namespace()
-		server.BroadcastToRoom(nameSpace, "room1", "typing", messageText)
-		return messageText
+		server.BroadcastToRoom(nameSpace, "room1", "broadcastAll", messageText)
 	})
 
 	// handling socket event error
 	server.OnError("/chat", func(so socketio.Conn, message error) {
 		// print on console log server
-		// messageError := message
-		// fmt.Println("error: ", messageError)
-		fmt.Println("error: ", message)
+		// fmt.Println("error: ", message)
+
+		messageError := message
+		fmt.Println("error: ", messageError)
 	})
 
 	// handling socket event disconnected
 	server.OnDisconnect("/chat", func(so socketio.Conn, reason string) {
 		// print on console log server
-		// messageReason := reason
-		// fmt.Println("closed: ", messageReason)
-		fmt.Println("closed: ", reason)
+		// fmt.Println("closed: ", reason)
+
+		messageReason := reason
+		fmt.Println("closed: ", messageReason)
 	})
 
 	go server.Serve()
 	defer server.Close()
 
-	http.Handle("/", server)
+	http.Handle("/socket.io/", server)
 	http.Handle("/", http.FileServer(http.Dir("./asset")))
 	log.Println("serving localhost on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
